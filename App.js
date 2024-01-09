d1d9531d01d7ee39d527e81c42abfadcbf8d7700
abdulahad21 <82265672+abdulahad21@users.noreply.github.com>
2024-01-09 15:03:12 +0500

    Add Router

---
+++ App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Student from './components/StudentForm';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import About from './components/About';
+
+function App() {
+  const[mode,SetMode] = useState('light');
+  const[textmode,SetTextMode] = useState('dark');
+  const[textcolor,SetTextcolor] = useState('dark');
+
+  const toggleMode = () =>{
+      if(mode === 'light')
+      {
+        SetMode('dark');
+        SetTextMode('light');
+        SetTextcolor('light');
+      }
+      else
+      {
+        SetMode('light');
+        SetTextMode('dark');
+        SetTextcolor('dark');
+      }
+  }
+  return (
+    <>
+   
+      <Router>
+        <Nav
+          Title="Student Info"
+          Second="Home"
+          Third="About"
+          toggleMode={toggleMode}
+          mode={mode}
+          txtmode={textmode}
+          textcolor={textcolor}
+        />
+         <div className="container my-3">
+          <Routes>
+            <Route>
+              <Route path="/" element={<Student />} />
+              <Route path="/StudentForm" element={<Student />} />
+              <Route path="/About" element={<About />} />
+            </Route>
+          </Routes>
+          </div>
+      </Router>
+     
+    </>
+  );
+}
+
+export default App;
